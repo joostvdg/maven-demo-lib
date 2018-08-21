c6b7a93de69c741f30bf0ef0418d116ce283811b
Joost van der Griendt <joostvdg@gmail.com>
2018-08-21 22:39:53 +0200

    Get stuff in order

---
+++ src/main/java/com/github/joostvdg/demo/Hello.java
@@ -0,0 +1,11 @@
+package com.github.joostvdg.demo;
+
+/**
+ * This class is intended to say Hello.
+ */
+public class Hello {
+
+    public String hello() {
+        return "Hello world!";
+    }
+}
